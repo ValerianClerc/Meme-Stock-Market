6cc13baaae74e540fbe4b7ba8f16acc9e75f273c
Valerian Clerc <valerian.clerc@mail.mcgill.ca>
2018-07-23 18:17:32 -0400

    set up meme db

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var express     = require('express'),
     mongoose    = require('mongoose'),
     bodyParser  = require('body-parser')
 
+    var faker = require('faker');
+
 var app = express();
 
 // app set-up
@@ -10,7 +12,25 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended : true}));
 
 // MongoDB set-up
-mongoose.connect('mongodb://localhost:27017/blog-app', {useNewUrlParser:true});
+mongoose.connect('mongodb://localhost:27017/meme-stock-market', {useNewUrlParser:true});
+
+var memeSchema = new mongoose.Schema({
+    title: String,
+    imgURL: String,
+    timeStamp: String,
+    author: String
+});
+
+var Meme = mongoose.model("Meme", memeSchema);
+
+// Meme.create({
+//     title: faker.commerce.productName(),
+//     imgURL: faker.image.avatar(),
+//     timeStamp: faker.date.past(),
+//     author: faker.internet.userName()
+// }, function (err, meme){
+//     console.log(meme);
+// });
 
 app.get('/', function (req, res) {
     res.render('index');
@@ -37,7 +57,13 @@ app.get('/signup', function (req, res) {
 });
 
 app.get('/topMemes', function (req, res) {
-    res.render('topMemes');
+    Meme.find({}, function (err, memes) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render('topMemes', {memes: memes});
+        }
+    });
 });
 
 app.listen(3000, function () {
