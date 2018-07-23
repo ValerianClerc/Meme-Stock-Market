7e25708af9cb8940bf54521d44cee5d830aeb367
Valerian Clerc <valerian.clerc@mail.mcgill.ca>
2018-07-23 13:34:31 -0400

    refactor for express

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Meme-Stock-Market
+
+Idea: a platform for sharing memes, gaining points for popular memes, and reinvesting these points into meme shares.
+
+See ```package.json``` for dependencies.
--- app.js
+++ app.js
@@ -5,9 +5,41 @@ var express     = require('express'),
 var app = express();
 
 // app set-up
-app.use(express.static('public/css'));
+app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended : true}));
 
 // MongoDB set-up
 mongoose.connect('mongodb://localhost:27017/blog-app', {useNewUrlParser:true});
+
+app.get('/', function (req, res) {
+    res.render('index');
+});
+
+app.get('/about', function (req, res) {
+    res.render('about');
+});
+
+app.get('/FAQ', function (req, res) {
+    res.render('FAQ');
+});
+
+app.get('/guide', function (req, res) {
+    res.render('guide');
+});
+
+app.get('/login', function (req, res) {
+    res.render('login');
+});
+
+app.get('/signup', function (req, res) {
+    res.render('signup');
+});
+
+app.get('/topMemes', function (req, res) {
+    res.render('topMemes');
+});
+
+app.listen(3000, function () {
+    console.log('Server started on port 3000');
+});
