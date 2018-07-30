9835800d54364f6c024bba0cabd43bb2e2fad0a1
MohamedBeydoun <mohamed.beydoun@mail.mcgill.ca>
2018-07-30 15:30:07 -0400

    like and read more button styling

--- public/javascript/posts.js
+++ public/javascript/posts.js
@@ -2,6 +2,7 @@ $(function () {
     $('#grid').on('click', 'button', function () {
         var $id = JSON.parse($('.data').attr('data-test-value'))._id;
         $(this).toggleClass('liked');
+        $(this).toggleClass('not-liked');
         $.ajax({
             type: 'POST',
             url: '../posts/' + $id + '/like',
