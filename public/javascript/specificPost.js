173666e21a41726025fc73d832d7c73b9628f3f0
MohamedBeydoun <mohamed.beydoun@mail.mcgill.ca>
2018-07-31 03:09:53 -0400

    styled specific post and new post

--- public/javascript/specificPost.js
+++ public/javascript/specificPost.js
@@ -2,6 +2,7 @@ $(function () {
     $('#like').on('click', function () {
         var $id = JSON.parse($('.data').attr('data-test-value'))._id;
         $(this).toggleClass('liked');
+        $(this).toggleClass('not-liked');
         $.ajax({
             type: 'POST',
             url: '../posts/' + $id + '/like',
