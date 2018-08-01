0023bb8b97bae2258bdcc10fc6deb00a5f917182
Valerian Clerc <valerian.clerc@mail.mcgill.ca>
2018-08-01 16:56:56 -0400

    cleaned up like counter update and added it to posts page

--- public/javascript/posts.js
+++ public/javascript/posts.js
@@ -1,13 +1,18 @@
 $(function () {
-    $('#grid').on('click', 'button', function () {
+    $('#grid').on('click', '#like', function () {
         var $id = JSON.parse($('.data').attr('data-test-value'))._id;
         $(this).toggleClass('liked');
         $(this).toggleClass('not-liked');
         $.ajax({
+            context: this,
             type: 'POST',
             url: '../posts/' + $id + '/like',
+            datatype: 'json',
             error: function () {
                 alert('error');
+            },
+            success: function (response) {
+                $(this).parent().find('#likeCount span').html(JSON.parse(response).total);
             }
         });
     });
--- public/javascript/specificPost.js
+++ public/javascript/specificPost.js
@@ -9,16 +9,8 @@ $(function () {
             error: function () {
                 alert('error');
             },
-            success: function () {
-                console.log(parseInt($('#likeCount span').text()));
-                var likes = parseInt($('#likeCount span').text(), 10);
-                console.log(likes);
-                if ($('#like').hasClass('liked')) {
-                    likes++;
-                } else {
-                    likes--;
-                }
-                $('#main #likeCount span').text(likes);
+            success: function (response) {
+                $('#main #likeCount span').html(JSON.parse(response).total);
             }
         });
     });
