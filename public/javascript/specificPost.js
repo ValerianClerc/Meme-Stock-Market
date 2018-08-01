965fb4fb07ef358b01797a8e2d164d7bdac53589
Valerian Clerc <valerian.clerc@mail.mcgill.ca>
2018-08-01 11:22:12 -0400

    added live updating like counter

--- public/javascript/specificPost.js
+++ public/javascript/specificPost.js
@@ -8,6 +8,17 @@ $(function () {
             url: '../posts/' + $id + '/like',
             error: function () {
                 alert('error');
+            },
+            success: function () {
+                console.log(parseInt($('#likeCount span').text()));
+                var likes = parseInt($('#likeCount span').text(), 10);
+                console.log(likes);
+                if ($('#like').hasClass('liked')) {
+                    likes++;
+                } else {
+                    likes--;
+                }
+                $('#main #likeCount span').text(likes);
             }
         });
     });
